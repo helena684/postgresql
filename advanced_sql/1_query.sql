0c7d898c50e863aa7efcdfe6b3c73c0e0c03a3b0
helena684 <cynthiahellen011@gmail.com>
2026-06-04 10:42:27 +0300

    queries

---
+++ advanced_sql/1_query.sql
@@ -0,0 +1,23 @@
+/*what are we solving? What are the top-paying jobs for my role(Data Analyst)?
+- Identify the 10 highest-paying Data Analyst roles that are available remotely
+- Focus on job postings with specified salaries(remove nulls)
+*/
+SELECT 
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM 
+    job_postings_fact
+
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND 
+    job_location ='Anywhere'AND
+    salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10
